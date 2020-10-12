7a1342b96b7db8c5db8821c992858d7d5efbcbb2
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-12 06:34:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,5 +11,5 @@ class Anagram
   end
 end
 
-listen = Anagram.new("listen"
-listen.match(%w(enlists google inlets banana))
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana)
